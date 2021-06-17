161d54d59050a7c21f88e7d4bc3cc7b8be5a5b29
spfave <sebpfave@gmail.com>
2021-06-16 23:40:05 -0400

    Rename homeRoutes to viewRoutes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./viewRoutes');
 // const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
