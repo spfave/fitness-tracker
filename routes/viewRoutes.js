5627850348ed8cc460ec23e415ae90b62fb90eb7
spfave <sebpfave@gmail.com>
2021-06-16 23:52:37 -0400

    Add comments

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-// const path = require('path');
-
 const { viewController } = require('../controllers');
 
+// Homepage
 router.get('/', viewController.viewHomePage);
 
+// Exercise page
 router.get('/exercise', viewController.viewExercisePage);
 
+// Stats page
 router.get('/stats', viewController.viewStatsPage);
 
 module.exports = router;
