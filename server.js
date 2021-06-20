cb3324342452329f66d08941212ae84db5e5c5ad
Sebastian F <sebpfave2@gmail.com>
2021-06-19 20:31:07 -0400

    Setup server db connection

--- server.js
+++ server.js
@@ -26,9 +26,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // Database connection
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutDB', {
-//   useNewUrlParser: true,
-// });
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutDB', {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('MongoDB connected'));
 
 // Start server
 app.listen(PORT, () => console.log(`App running on port ${PORT}!`));
