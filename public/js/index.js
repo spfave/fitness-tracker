b33c5b10036eb62db5043a405f2fde59cb1a7973
Sebastian F <sebpfave2@gmail.com>
2021-06-19 22:34:32 -0400

    Remove old code

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,6 @@ async function init() {
     if (workout) {
       location.search = '?id=' + workout._id;
     } else {
-      // document.querySelector('#continue-btn').classList.add('d-none');
       document.querySelector('#continue-btn').classList.remove('blue');
       document.querySelector('#continue-btn').classList.add('anchor-disable');
     }
